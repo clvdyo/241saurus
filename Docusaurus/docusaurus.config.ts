24cdcec3c39d52936da1a6ad8408f048321dff00
clvdyo <114920053+clvdyo@users.noreply.github.com>
2024-04-24 14:23:46 +0700

    popquiz

--- Docusaurus/docusaurus.config.ts
+++ Docusaurus/docusaurus.config.ts
@@ -12,17 +12,6 @@ const config: Config = {
   projectName: '241saurus',         // (Usually your repo name)
   trailingSlash: true,              // "true" or "false"
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
